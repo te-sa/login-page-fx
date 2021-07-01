2978ed40d91c86f566421813492718b214d378f1
te-sa <teresa_saller@freenet.de>
2021-07-01 13:28:58 +0200

    working on logout

--- src/Page.java
+++ src/Page.java
@@ -1,9 +1,26 @@
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Page {
     @FXML
     Label label;
+    @FXML
+    Button logoutButton;
+
+    @FXML
+    void logOut() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+        Parent root = loader.load();
+        Stage stage = (Stage) logoutButton.getScene().getWindow();
+        stage.setScene(new Scene(root));
+    }
 
     void greeting(String username) {
         label.setText("Hello " + username + "!");

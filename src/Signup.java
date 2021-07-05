6341c9f248da559baed622c34f2eb7193eb553a5
te-sa <teresa_saller@freenet.de>
2021-07-05 11:31:42 +0200

    tried to unify layouts of login and signup

--- src/Signup.java
+++ src/Signup.java
@@ -3,11 +3,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Signup {
+    @FXML
+    TextField usernameField;
+    @FXML
+    PasswordField passwordField;
+    @FXML
+    PasswordField confirmPasswordField;
     @FXML
     Button backButton;
 

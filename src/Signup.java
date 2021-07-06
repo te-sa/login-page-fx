5246d76dfa00092553e1f71fc13621b29735582b
te-sa <teresa_saller@freenet.de>
2021-07-06 17:10:01 +0200

    worked on signup methods and fxml files

--- src/Signup.java
+++ src/Signup.java
@@ -8,16 +8,61 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Signup {
     @FXML
-    TextField usernameField;
+    private TextField usernameField;
     @FXML
-    PasswordField passwordField;
+    private PasswordField passwordField;
     @FXML
-    PasswordField confirmPasswordField;
+    private PasswordField confirmPasswordField;
     @FXML
-    Button backButton;
+    private Button signUpButton;
+    @FXML
+    private Button backButton;
+
+    public void setUsername(String username) {
+
+    }
+
+    public void setPassword(String password) {
+
+    }
+
+    private boolean availableUsername() {
+        return true;
+    }
+
+    private boolean validPassword() {
+        String potentialPassword = passwordField.getText();
+
+        Pattern specialChars = Pattern.compile("[^a-zA-z0-9 ]");
+        Pattern numbers = Pattern.compile("[0-9]");
+
+        Matcher matchSpecial = specialChars.matcher(potentialPassword);
+        Matcher matchNums = numbers.matcher(potentialPassword);
+
+        boolean longEnough = potentialPassword.length() >= 8;
+        boolean hasUpperCase = !potentialPassword.toLowerCase().equals(potentialPassword);
+        boolean hasLowerCase = !potentialPassword.toUpperCase().equals(potentialPassword);
+        boolean hasNum = matchNums.find();
+        boolean hasSpecial = matchSpecial.find();
+
+        return longEnough && hasUpperCase && hasLowerCase && hasNum && hasSpecial;
+    }
+
+    private boolean passwordsMatch() {
+        return passwordField.getText().equals(confirmPasswordField.getText());
+    }
+
+    public void signUp() {
+        if (availableUsername() && validPassword() && passwordsMatch()) {
+            setUsername(usernameField.getText());
+            setPassword(passwordField.getText());
+        }
+    }
 
     @FXML
     public void switchToLogin() throws IOException {
